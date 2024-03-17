c545539ddd303421012d8d33a677c143b6411ae8
jennet-zamanova <zamanova@mit.edu>
2024-03-17 10:28:52 -0400

    made GeoJSON somewhat work

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
+import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from "react-leaflet";
+import { map_1980 } from "./1980.js";
+import "leaflet/dist/leaflet.css";
+import "./Map.css";
 
 function Map() {
-  const position = [51.505, -0.09];
-
+  console.log(map_1980);
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
-    </MapContainer>
+    <div className="Map">
+      <MapContainer
+        center={[42.35346337378607, -71.14454379278231]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="Map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON data={map_1980} style={{ fillColor: "red" }}></GeoJSON>
+        <Marker position={[42.35346337378607, -71.14454379278231]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
